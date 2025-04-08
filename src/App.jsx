189e12222d278fb8c78c8909f5a006ca42efc4ad
Evrp <161710768+Evrp@users.noreply.github.com>
2025-04-08 20:35:18 +0700

    แก้ใหม่

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Navbar";
 import Profile from "./Home/Profile";
-import Food from "./Home/Food";
 import LoginForm from "./Log/NewLogin";
 import Community from "./Home/Newcommu";
 import Home from "./Home/Home";
@@ -10,17 +9,17 @@ import { Route, Routes, useLocation } from "react-router-dom";
 // import ForgotPassword from "./Log/ForgotForm";
 
 function App() {
-  const location = useLocation();  // ใช้เพื่อดึงข้อมูล path ปัจจุบัน
+  const location = useLocation(); // ใช้เพื่อดึงข้อมูล path ปัจจุบัน
 
   return (
     <>
       {/* หากไม่ใช่หน้า login, จะแสดง Navbar */}
       {location.pathname !== "/login" && <Navbar />}
-      
+
       <Routes>
         <Route path="/hone" element={<Home />} />
         <Route path="/login" element={<LoginForm />} />
-        <Route path="/food" element={<Food />} />
+
         {/* <Route path="/forgot-password" element={<ForgotPassword />} /> */}
         <Route path="/profile" element={<Profile />} />
         <Route path="/community" element={<Community />} />
