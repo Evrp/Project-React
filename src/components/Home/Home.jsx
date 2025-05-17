49a14f4b6df3952d2442c7ba81fba1ed9e3beebf
Evrp <161710768+Evrp@users.noreply.github.com>
2025-05-18 00:10:46 +0700

    uiui

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import './Home.css';
-import { EventContext } from "../../context/eventcontext";
-import { useContext } from "react";
+import "./Home.css";
 import EventList from "../ui/Eventlist";
 
 const Newcommu = () => {
   const userPhoto = localStorage.getItem("userPhoto");
   return (
-    <div className="main-content">
+    <div className="main-contentnn">
       <div className="header">
-        <h1></h1>
         <div className="profile-section">
           <span className="bell-icon">&#128276;</span>
           <span className="divider">|</span>
-          <img
-            src={userPhoto}
-            alt="Profile"
-            className="profile-image-com"
-          />
+          <img src={userPhoto} alt="Profile" className="profile-image-com" />
         </div>
       </div>
-    
+
       <div className="event-list">
         <EventList />
       </div>
